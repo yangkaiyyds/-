185557537db365448d3767c68b99c84fe0bf28b7
nbjd <nbjd@qq.com>
2021-09-20 23:18:31 +0800

    提交

---
+++ student/src/student/student.java
@@ -0,0 +1,16 @@
+package student;
+
+public class student {
+    student hh=new student();
+	String name;
+	int year;
+	int high;
+	hh.name=张三；
+	hh.year=18;
+	hh.high=178;
+}
+public static void main(String srgs[]) {
+	System.out.println("学生姓名是"+hh.name);
+	System.out.println("学生年龄是"+hh,year);
+	System.out.println("学生身高是"+hh.high);
+}
