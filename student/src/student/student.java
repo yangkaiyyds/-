2435ab814e2c60a9aebdc36e7dcba497f7faf18b
nbjd <nbjd@qq.com>
2021-09-20 23:48:05 +0800

    student修改

--- student/src/student/student.java
+++ student/src/student/student.java
@@ -1,6 +1,7 @@
 package student;
 
 public class student {
+	public static void main(String[] args) {
     student hh=new student();
 	String name;
 	int year;
@@ -8,9 +9,8 @@ public class student {
 	hh.name=张三；
 	hh.year=18;
 	hh.high=178;
-}
-public static void main(String srgs[]) {
 	System.out.println("学生姓名是"+hh.name);
 	System.out.println("学生年龄是"+hh,year);
 	System.out.println("学生身高是"+hh.high);
+}
 }
