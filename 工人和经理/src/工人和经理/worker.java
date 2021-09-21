e8a5dcdc10bf137f0658b46d4a83ccf66b14776b
nbjd <nbjd@qq.com>
2021-09-21 19:26:39 +0800

    更改工人经理

--- 工人和经理/src/工人和经理/worker.java
+++ 工人和经理/src/工人和经理/worker.java
@@ -27,18 +27,10 @@ public worker(String name,int salary,int day) {
 	this.salary=salary;
 	this.day=day;
 }
-public worker() {
-	this.name="张三";
-	this.salary=250;
-	this.day=30;
+public void all() {
+	 this.salary=this.salary*this.day;
 }
-public int getall() {
-	return salary*day;
-}
-public void main(String[] args) {
-	worker r1=new worker(this.name,this.salary,this.day);
-	System.out.println("姓名"+r1.name);
-	System.out.println("日工资"+r1.salary);
-	System.out.println("月工资"+r1.getall());
+public void print() {
+	System.out.println(name+"每月工资:"+salary);
 }
 }
